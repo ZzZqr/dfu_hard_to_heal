1fc55bdc22138625b1f25e37fdf473c9397b1142
zhengqirui <201250229@smail.nju.edu.cn>
2024-01-07 21:37:21 +0800

    add words

--- hello_azure/views.py
+++ hello_azure/views.py
@@ -20,15 +20,15 @@ def predict(request):
         age = int(request.POST.get('Age'))
 
         if age == 10:
-            probability = 0.1
+            probability = 0.10
         elif age == 20:
-            probability = 0.2
+            probability = 0.20
         elif age == 30:
-            probability = 0.5
+            probability = 0.50
         elif age == 40:
-            probability = 0.7
+            probability = 0.70
         elif age == 50:
-            probability = 0.9
+            probability = 0.90
 
         data = {'label': 0, 'probability': probability}
         return JsonResponse(data)
