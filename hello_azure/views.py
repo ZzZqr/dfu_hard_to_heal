c955f01267a4181679afbc9de8d98ec5cd6d587a
czf <2765260638@qq.com>
2024-01-07 20:30:38 +0800

    更改桩程序

--- hello_azure/views.py
+++ hello_azure/views.py
@@ -13,8 +13,24 @@ def index(request):
 @csrf_exempt
 def predict(request):
     if request.method == 'POST':
-        time.sleep(5)
-        data = {'label': 0, 'probability': 0.55}
+        time.sleep(3)
+
+        probability = 0.1
+
+        age = int(request.POST.get('Age'))
+
+        if age == 10:
+            probability = 0.1
+        elif age == 20:
+            probability = 0.2
+        elif age == 30:
+            probability = 0.5
+        elif age == 40:
+            probability = 0.7
+        elif age == 50:
+            probability = 0.9
+
+        data = {'label': 0, 'probability': probability}
         return JsonResponse(data)
     else:
         return render(request, 'hello_azure/index.html')
